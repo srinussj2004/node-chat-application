e4eb036b2097fbac0c434f890b2eb24c2f514af4
ARTHAM <sa458e@att.com>
2018-03-28 23:15:43 -0700

    add form and jquery

--- server/server.js
+++ server/server.js
@@ -21,10 +21,11 @@ io.on('connection', (socket) =>{
 
   socket.broadcast.emit('newMessage',generateMessage('Admin', 'New User Joined'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage',message);
 
     io.emit('newMessage',generateMessage(message.from, message.text));
+    callback(message.from);
 
     // socket.broadcast.emit('newMessage',{
     //     from: message.from,
