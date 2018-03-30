4f43daba37a790dd1f42b65073ae035ed514b5aa
ARTHAM <sa458e@att.com>
2018-03-30 16:06:54 -0700

    auto scrolling of chat page

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,22 @@
 var socket = io();
+
+function scrollToBottom(){
+  //Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  // Hieghts
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight =  messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight>= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
+
 socket.on('connect', function () {
   console.log('connected to the server');
 });
@@ -17,6 +35,7 @@ socket.on('newMessage', function(message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 
 
     // var li = jQuery('<li></li>');
@@ -36,6 +55,7 @@ socket.on('newLocationMessage',function(message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
     // var li = jQuery('<li></li>');
     // var a = jQuery('<a target="_blank">My Current Location</a>');
     // li.text(`${message.from} ${formattedTime}: `);
